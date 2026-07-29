0xhas/harbor

feat(ui): port liquid glass controls to beta (#1067)
* feat(ui): port liquid glass controls to beta

* fix(ui): polish liquid glass controls

* fix(ui): reset poster dock on interaction

src/lib/i18n/locales/pt/settings-fill.ts
@@ -139,6 +139,9 @@ const settingsFill: Record<string, string> = {
   "Resolution posters are decoded at. High is sized to your screen with headroom and looks identical to full res while using far less memory; Balanced saves the most; Maximum keeps original resolution.": "A resolução em que os pôsteres são decodificados. Alta é dimensionada para a sua tela com folga e parece idêntica à resolução total usando muito menos memória; Equilibrada é a que mais economiza; Máxima mantém a resolução original.",
   "Poster dock magnification": "Ampliação do dock de pôsteres",
   "Gently magnify nearby posters as you move across a poster row, like a dock. Off by default.": "Amplia suavemente os pôsteres próximos conforme você percorre uma linha de pôsteres, como um dock. Desativado por padrão.",
+  "Liquid Glass": "Vidro líquido",
+  "Use liquid glass": "Usar vidro líquido",
+  "Use liquid glass for the search pill and row scroll arrows. The appearance settings below are shared by glass surfaces across Harbor.": "Use vidro líquido na barra de pesquisa e nas setas de rolagem das fileiras. As configurações de aparência abaixo são compartilhadas pelas superfícies de vidro do Harbor.",
   "Enhanced liquid glass": "Vidro líquido aprimorado",
   "A richer glass treatment. May look better while using more graphics resources.": "Um efeito de vidro mais rico. Pode parecer melhor, mas consome mais recursos gráficos.",
   "Glass opacity": "Opacidade do vidro",
@@ -162,8 +165,12 @@ const settingsFill: Record<string, string> = {
   "Tuck clean, native-looking window buttons into the top corner, with hover labels. On macOS they become traffic-light dots. Blends into Harbor while feeling like your system's own title bar.": "Encaixa botões de janela limpos e com aparência nativa no canto superior, com rótulos ao passar o mouse. No macOS, eles se tornam pontos de semáforo. Combina com o Harbor e ao mesmo tempo parece a barra de título do seu próprio sistema.",
   "Frost the top bar on scroll": "Deixar a barra superior fosca ao rolar",
   "As you scroll, the top bar frosts over the content beneath it. Off by default; it uses a blur, so leave it off on lower-end machines.": "Ao rolar, a barra superior fica fosca sobre o conteúdo abaixo dela. Desativado por padrão; usa desfoque, então deixe desativado em máquinas mais fracas.",
-  "Clean, transparent top bar": "Barra superior limpa e transparente",
-  "On by default. The top-bar icons sit clean over the artwork with no pill behind them, and Watch Together opens as a top-right panel. Turn it off to bring back the filled pill buttons.": "Ativado por padrão. Os ícones da barra superior ficam limpos sobre a arte, sem cápsula atrás deles, e o Assistir Juntos abre como um painel no canto superior direito. Desative para trazer de volta os botões de cápsula preenchidos.",
+  "Top-right controls": "Controles do canto superior direito",
+  "The operating system draws native window controls, so Harbor cannot change their appearance.": "O sistema operacional desenha os controles nativos da janela, portanto o Harbor não pode alterar sua aparência.",
+  "Choose how Watch Together and the minimize, maximize, and close buttons look. Liquid glass replaces the clean transparent controls.": "Escolha a aparência de Assistir juntos e dos botões minimizar, maximizar e fechar. O vidro líquido substitui os controles transparentes limpos.",
+  "Clean transparent": "Transparente limpo",
+  "Liquid glass": "Vidro líquido",
+  "Filled": "Preenchido",
   "Drag the window from anywhere": "Arrastar a janela de qualquer lugar",
   "Move Harbor by dragging any empty space on a page, not just the top bar. Leave this off to keep clicks inside pages from nudging the window.": "Mova o Harbor arrastando qualquer espaço vazio de uma página, não apenas a barra superior. Deixe desativado para evitar que cliques dentro das páginas movam a janela sem querer.",
   "Stream priority": "Prioridade de streams",

src/views/settings/theme-panel.tsx
@@ -2,7 +2,7 @@ import { AppWindow, Move, PanelTop, Sparkles } from "lucide-react";
 import { useSettings } from "@/lib/settings";
 import { FEATURED_CUSTOM_THEMES, THEME_PRESETS, type ThemeSettings } from "@/lib/theme";
 import { useT } from "@/lib/i18n";
-import { Section } from "./shared";
+import { Section, Segmented } from "./shared";
 import { NewBadge } from "./new-badge";
 import { BackgroundPicker } from "./theme-panel/background-picker";
 import { ColorThemeBody } from "./theme-panel/color-theme-body";
@@ -97,8 +97,8 @@ export function ThemePanel() {
         >
           <NativeTitleBarRow />
           <HybridBarRow />
-          <TopbarScrollBlurRow />
-          <CleanTopBarRow />
+          <TopbarAppearanceRow />
+          {settings.topbarAppearance !== "transparent" && <TopbarScrollBlurRow />}
         </Section>
       )}
 
@@ -262,40 +262,43 @@ function TopbarScrollBlurRow() {
   );
 }
 
-function CleanTopBarRow() {
+function TopbarAppearanceRow() {
   const t = useT();
   const { settings, update } = useSettings();
-  const on = settings.transparentTopBar;
+  const nativeOn = settings.useNativeTitleBar;
   return (
-    <div className="flex items-start gap-3 rounded-xl border border-edge-soft bg-canvas/40 px-4 py-3.5">
+    <div className={`flex items-start gap-3 rounded-xl border border-edge-soft bg-canvas/40 px-4 py-3.5 ${nativeOn ? "opacity-55" : ""}`}>
       <span
         className={`mt-0.5 flex h-9 w-9 shrink-0 items-center justify-center rounded-full ${
-          on ? "bg-accent/15 text-accent" : "bg-raised text-ink-subtle"
+          nativeOn ? "bg-raised text-ink-subtle" : "bg-accent/15 text-accent"
         }`}
       >
         <Sparkles size={15} strokeWidth={2.2} />
       </span>
       <div className="flex min-w-0 flex-1 flex-col gap-1">
-        <span className="text-[14px] font-medium text-ink">{t("Clean, transparent top bar")}</span>
+        <span className="text-[14px] font-medium text-ink">{t("Top-right controls")}</span>
         <p className="text-[12.5px] leading-relaxed text-ink-subtle">
-          {t("On by default. The top-bar icons sit clean over the artwork with no pill behind them, and Watch Together opens as a top-right panel. Turn it off to bring back the filled pill buttons.")}
+          {nativeOn
+            ? t("The operating system draws native window controls, so Harbor cannot change their appearance.")
+            : t("Choose how Watch Together and the minimize, maximize, and close buttons look. Liquid glass replaces the clean transparent controls.")}
         </p>
+        <div className={nativeOn ? "pointer-events-none mt-2" : "mt-2"}>
+          <Segmented
+            value={settings.topbarAppearance}
+            options={[
+              { value: "transparent", label: t("Clean transparent") },
+              { value: "glass", label: t("Liquid glass") },
+              { value: "filled", label: t("Filled") },
+            ]}
+            onChange={(topbarAppearance) =>
+              update({
+                topbarAppearance,
+                transparentTopBar: topbarAppearance === "transparent",
+              })
+            }
+          />
+        </div>
       </div>
-      <button
-        type="button"
-        role="switch"
-        aria-checked={on}
-        onClick={() => update({ transparentTopBar: !on })}
-        className={`mt-1 flex h-6 w-11 shrink-0 items-center rounded-full px-0.5 transition-colors ${
-          on ? "bg-accent" : "bg-raised"
-        }`}
-      >
-        <span
-          className={`h-5 w-5 rounded-full bg-canvas shadow-sm transition-transform ${
-            on ? "translate-x-5 rtl:-translate-x-5" : "translate-x-0"
-          }`}
-        />
-      </button>
     </div>
   );
 }

src/views/settings/theme-panel/display-section.tsx
@@ -15,48 +15,16 @@ export function DisplaySection() {
   return (
     <>
       <PosterCardSection previewPoster={previewPoster} />
-      <ToggleRow
-        label={t("Liquid glass row arrows")}
-        newId="theme:liquid-glass"
-        sub={t("Render the row scroll arrows as a refracting liquid-glass button. Off by default; needs WebGL and falls back automatically.")}
-        value={settings.liquidGlass}
-        onChange={(v) => update({ liquidGlass: v })}
-      />
-      <ToggleRow
-        label={t("Poster dock magnification")}
-        newId="theme:poster-dock"
-        sub={t("Gently magnify nearby posters as you move across a poster row, like a dock. Off by default.")}
-        value={settings.posterDockMagnification}
-        onChange={(posterDockMagnification) => update({ posterDockMagnification })}
-      />
-      {settings.posterDockMagnification && (
-        <div className="flex items-center gap-4 px-1 py-1.5">
-          <span className="w-32 shrink-0 text-[13.5px] font-medium text-ink">{t("Animation speed")}</span>
-          <input
-            type="range"
-            min="250"
-            max="1500"
-            step="10"
-            value={settings.posterDockTransitionMs}
-            onChange={(e) => update({ posterDockTransitionMs: Number(e.target.value) })}
-            className="h-1 flex-1 appearance-none rounded-full bg-edge-soft accent-ink"
-          />
-          <span className="w-16 shrink-0 text-end text-[13px] tabular-nums text-ink-muted">
-            {settings.posterDockTransitionMs}ms
-          </span>
-          {settings.posterDockTransitionMs !== 760 && (
-            <button
-              type="button"
-              onClick={() => update({ posterDockTransitionMs: 760 })}
-              className="shrink-0 text-[12.5px] font-medium text-ink-subtle transition-colors hover:text-ink"
-            >
-              {t("Reset")}
-            </button>
-          )}
-        </div>
-      )}
-      {settings.liquidGlass && (
-        <>
+      <Section title={t("Liquid Glass")}>
+        <ToggleRow
+          label={t("Use liquid glass")}
+          newId="theme:liquid-glass"
+          sub={t("Use liquid glass for the search pill and row scroll arrows. The appearance settings below are shared by glass surfaces across Harbor.")}
+          value={settings.liquidGlass}
+          onChange={(v) => update({ liquidGlass: v })}
+        />
+        {settings.liquidGlass && (
+          <>
           <ToggleRow
             label={t("Enhanced liquid glass")}
             sub={t("A richer glass treatment. May look better while using more graphics resources.")}
@@ -109,8 +77,9 @@ export function DisplaySection() {
               </div>
             </>
           )}
-        </>
-      )}
+          </>
+        )}
+      </Section>
 
       <Section
         title={t("Sound effects")}
@@ -399,4 +368,3 @@ function SizeSlider({
     </div>
   );
 }
-

2-sa/Bear

src/lib/i18n/locales/pt/settings-fill.ts
@@ -139,6 +139,9 @@ const settingsFill: Record<string, string> = {
   "Resolution posters are decoded at. High is sized to your screen with headroom and looks identical to full res while using far less memory; Balanced saves the most; Maximum keeps original resolution.": "A resolução em que os pôsteres são decodificados. Alta é dimensionada para a sua tela com folga e parece idêntica à resolução total usando muito menos memória; Equilibrada é a que mais economiza; Máxima mantém a resolução original.",
   "Poster dock magnification": "Ampliação do dock de pôsteres",
   "Gently magnify nearby posters as you move across a poster row, like a dock. Off by default.": "Amplia suavemente os pôsteres próximos conforme você percorre uma linha de pôsteres, como um dock. Desativado por padrão.",
+  "Liquid Glass": "Vidro líquido",
+  "Use liquid glass": "Usar vidro líquido",
+  "Use liquid glass for the search pill and row scroll arrows. The appearance settings below are shared by glass surfaces across Bear.": "Use vidro líquido na barra de pesquisa e nas setas de rolagem das fileiras. As configurações de aparência abaixo são compartilhadas pelas superfícies de vidro do Bear.",
   "Enhanced liquid glass": "Vidro líquido aprimorado",
   "A richer glass treatment. May look better while using more graphics resources.": "Um efeito de vidro mais rico. Pode parecer melhor, mas consome mais recursos gráficos.",
   "Glass opacity": "Opacidade do vidro",
@@ -162,8 +165,12 @@ const settingsFill: Record<string, string> = {
   "Tuck clean, native-looking window buttons into the top corner, with hover labels. On macOS they become traffic-light dots. Blends into Bear while feeling like your system's own title bar.": "Encaixa botões de janela limpos e com aparência nativa no canto superior, com rótulos ao passar o mouse. No macOS, eles se tornam pontos de semáforo. Combina com o Bear e ao mesmo tempo parece a barra de título do seu próprio sistema.",
   "Frost the top bar on scroll": "Deixar a barra superior fosca ao rolar",
   "As you scroll, the top bar frosts over the content beneath it. Off by default; it uses a blur, so leave it off on lower-end machines.": "Ao rolar, a barra superior fica fosca sobre o conteúdo abaixo dela. Desativado por padrão; usa desfoque, então deixe desativado em máquinas mais fracas.",
-  "Clean, transparent top bar": "Barra superior limpa e transparente",
-  "On by default. The top-bar icons sit clean over the artwork with no pill behind them, and Watch Together opens as a top-right panel. Turn it off to bring back the filled pill buttons.": "Ativado por padrão. Os ícones da barra superior ficam limpos sobre a arte, sem cápsula atrás deles, e o Assistir Juntos abre como um painel no canto superior direito. Desative para trazer de volta os botões de cápsula preenchidos.",
+  "Top-right controls": "Controles do canto superior direito",
+  "The operating system draws native window controls, so Bear cannot change their appearance.": "O sistema operacional desenha os controles nativos da janela, portanto o Bear não pode alterar sua aparência.",
+  "Choose how Watch Together and the minimize, maximize, and close buttons look. Liquid glass replaces the clean transparent controls.": "Escolha a aparência de Assistir juntos e dos botões minimizar, maximizar e fechar. O vidro líquido substitui os controles transparentes limpos.",
+  "Clean transparent": "Transparente limpo",
+  "Liquid glass": "Vidro líquido",
+  "Filled": "Preenchido",
   "Drag the window from anywhere": "Arrastar a janela de qualquer lugar",
   "Move Bear by dragging any empty space on a page, not just the top bar. Leave this off to keep clicks inside pages from nudging the window.": "Mova o Bear arrastando qualquer espaço vazio de uma página, não apenas a barra superior. Deixe desativado para evitar que cliques dentro das páginas movam a janela sem querer.",
   "Stream priority": "Prioridade de streams",

src/views/settings/theme-panel.tsx
@@ -2,7 +2,7 @@ import { AppWindow, Move, PanelTop, Sparkles } from "lucide-react";
 import { useSettings } from "@/lib/settings";
 import { FEATURED_CUSTOM_THEMES, THEME_PRESETS, type ThemeSettings } from "@/lib/theme";
 import { useT } from "@/lib/i18n";
-import { Section } from "./shared";
+import { Section, Segmented } from "./shared";
 import { NewBadge } from "./new-badge";
 import { BackgroundPicker } from "./theme-panel/background-picker";
 import { ColorThemeBody } from "./theme-panel/color-theme-body";
@@ -104,8 +104,8 @@ export function ThemePanel() {
         >
           <NativeTitleBarRow />
           <HybridBarRow />
-          <TopbarScrollBlurRow />
-          <CleanTopBarRow />
+          <TopbarAppearanceRow />
+          {settings.topbarAppearance !== "transparent" && <TopbarScrollBlurRow />}
         </Section>
       )}
 
@@ -269,40 +269,43 @@ function TopbarScrollBlurRow() {
   );
 }
 
-function CleanTopBarRow() {
+function TopbarAppearanceRow() {
   const t = useT();
   const { settings, update } = useSettings();
-  const on = settings.transparentTopBar;
+  const nativeOn = settings.useNativeTitleBar;
   return (
-    <div className="flex items-start gap-3 rounded-xl border border-edge-soft bg-canvas/40 px-4 py-3.5">
+    <div className={`flex items-start gap-3 rounded-xl border border-edge-soft bg-canvas/40 px-4 py-3.5 ${nativeOn ? "opacity-55" : ""}`}>
       <span
         className={`mt-0.5 flex h-9 w-9 shrink-0 items-center justify-center rounded-full ${
-          on ? "bg-accent/15 text-accent" : "bg-raised text-ink-subtle"
+          nativeOn ? "bg-raised text-ink-subtle" : "bg-accent/15 text-accent"
         }`}
       >
         <Sparkles size={15} strokeWidth={2.2} />
       </span>
       <div className="flex min-w-0 flex-1 flex-col gap-1">
-        <span className="text-[14px] font-medium text-ink">{t("Clean, transparent top bar")}</span>
+        <span className="text-[14px] font-medium text-ink">{t("Top-right controls")}</span>
         <p className="text-[12.5px] leading-relaxed text-ink-subtle">
-          {t("On by default. The top-bar icons sit clean over the artwork with no pill behind them, and Watch Together opens as a top-right panel. Turn it off to bring back the filled pill buttons.")}
+          {nativeOn
+            ? t("The operating system draws native window controls, so Bear cannot change their appearance.")
+            : t("Choose how Watch Together and the minimize, maximize, and close buttons look. Liquid glass replaces the clean transparent controls.")}
         </p>
+        <div className={nativeOn ? "pointer-events-none mt-2" : "mt-2"}>
+          <Segmented
+            value={settings.topbarAppearance}
+            options={[
+              { value: "transparent", label: t("Clean transparent") },
+              { value: "glass", label: t("Liquid glass") },
+              { value: "filled", label: t("Filled") },
+            ]}
+            onChange={(topbarAppearance) =>
+              update({
+                topbarAppearance,
+                transparentTopBar: topbarAppearance === "transparent",
+              })
+            }
+          />
+        </div>
       </div>
-      <button
-        type="button"
-        role="switch"
-        aria-checked={on}
-        onClick={() => update({ transparentTopBar: !on })}
-        className={`mt-1 flex h-6 w-11 shrink-0 items-center rounded-full px-0.5 transition-colors ${
-          on ? "bg-accent" : "bg-raised"
-        }`}
-      >
-        <span
-          className={`h-5 w-5 rounded-full bg-canvas shadow-sm transition-transform ${
-            on ? "translate-x-5 rtl:-translate-x-5" : "translate-x-0"
-          }`}
-        />
-      </button>
     </div>
   );
 }

src/views/settings/theme-panel/display-section.tsx
@@ -15,48 +15,16 @@ export function DisplaySection() {
   return (
     <>
       <PosterCardSection previewPoster={previewPoster} />
-      <ToggleRow
-        label={t("Liquid glass row arrows")}
-        newId="theme:liquid-glass"
-        sub={t("Render the row scroll arrows as a refracting liquid-glass button. Off by default; needs WebGL and falls back automatically.")}
-        value={settings.liquidGlass}
-        onChange={(v) => update({ liquidGlass: v })}
-      />
-      <ToggleRow
-        label={t("Poster dock magnification")}
-        newId="theme:poster-dock"
-        sub={t("Gently magnify nearby posters as you move across a poster row, like a dock. Off by default.")}
-        value={settings.posterDockMagnification}
-        onChange={(posterDockMagnification) => update({ posterDockMagnification })}
-      />
-      {settings.posterDockMagnification && (
-        <div className="flex items-center gap-4 px-1 py-1.5">
-          <span className="w-32 shrink-0 text-[13.5px] font-medium text-ink">{t("Animation speed")}</span>
-          <input
-            type="range"
-            min="250"
-            max="1500"
-            step="10"
-            value={settings.posterDockTransitionMs}
-            onChange={(e) => update({ posterDockTransitionMs: Number(e.target.value) })}
-            className="h-1 flex-1 appearance-none rounded-full bg-edge-soft accent-ink"
-          />
-          <span className="w-16 shrink-0 text-end text-[13px] tabular-nums text-ink-muted">
-            {settings.posterDockTransitionMs}ms
-          </span>
-          {settings.posterDockTransitionMs !== 760 && (
-            <button
-              type="button"
-              onClick={() => update({ posterDockTransitionMs: 760 })}
-              className="shrink-0 text-[12.5px] font-medium text-ink-subtle transition-colors hover:text-ink"
-            >
-              {t("Reset")}
-            </button>
-          )}
-        </div>
-      )}
-      {settings.liquidGlass && (
-        <>
+      <Section title={t("Liquid Glass")}>
+        <ToggleRow
+          label={t("Use liquid glass")}
+          newId="theme:liquid-glass"
+          sub={t("Use liquid glass for the search pill and row scroll arrows. The appearance settings below are shared by glass surfaces across Bear.")}
+          value={settings.liquidGlass}
+          onChange={(v) => update({ liquidGlass: v })}
+        />
+        {settings.liquidGlass && (
+          <>
           <ToggleRow
             label={t("Enhanced liquid glass")}
             sub={t("A richer glass treatment. May look better while using more graphics resources.")}
@@ -109,8 +77,9 @@ export function DisplaySection() {
               </div>
             </>
           )}
-        </>
-      )}
+          </>
+        )}
+      </Section>
 
       <Section
         title={t("Sound effects")}
@@ -399,4 +368,3 @@ function SizeSlider({
     </div>
   );
 }
-