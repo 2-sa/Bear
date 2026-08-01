0xhas/harbor

feat(linux): add native SVP playback

src/views/settings/anime-panel/svp-section.tsx
@@ -2,6 +2,7 @@ import { Check, ExternalLink, Loader2, Play } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useSettings } from "@/lib/settings";
 import { useT } from "@/lib/i18n";
+import { isLinuxDesktop } from "@/lib/platform";
 import { openUrl } from "@/lib/window";
 import { svpApply, svpLaunch, svpStatus, type SvpStatus } from "@/lib/svp";
 import { Section, ToggleRow, Segmented } from "../shared";
@@ -14,11 +15,16 @@ export function SvpSection() {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    svpStatus().then(setStatus).catch(() => {});
+    svpStatus()
+      .then(setStatus)
+      .catch(() => {});
   }, []);
 
   const installed = status?.installed ?? false;
   const ready = status?.ready ?? false;
+  const checking = status === null;
+  const supported = status?.supported ?? false;
+  const linux = isLinuxDesktop();
   const loadFailed = ready && status?.loadable === false;
 
   const openSvp = async () => {
@@ -43,7 +49,9 @@ export function SvpSection() {
     try {
       const vpy = await svpApply("60");
       update({ playerSvp: true, svpVpyPath: vpy });
-      svpStatus().then(setStatus).catch(() => {});
+      svpStatus()
+        .then(setStatus)
+        .catch(() => {});
     } catch (e) {
       setError(t("Couldn't set up SVP: {err}", { err: String(e) }));
     } finally {
@@ -54,31 +62,65 @@ export function SvpSection() {
   return (
     <Section
       title={t("SVP frame interpolation")}
-      subtitle={t("Genuine 48/60fps motion on anime, rendered right inside Harbor's player. SVP supplies the engine (VapourSynth + svpflow) and runs in your tray for licensing; Harbor's own player applies the interpolation, so it stays embedded and fully under your control. One-time install, then flip it on.")}
+      subtitle={
+        linux
+          ? t(
+              "Native 48/60fps motion through your Linux SVP and VapourSynth installation, rendered inside Harbor's embedded player.",
+            )
+          : t(
+              "Genuine 48/60fps motion on anime, rendered right inside Harbor's player. SVP supplies the engine (VapourSynth + svpflow) and runs in your tray for licensing; Harbor's own player applies the interpolation, so it stays embedded and fully under your control. One-time install, then flip it on.",
+            )
+      }
     >
       <Step n={1} title={t("SVP (free)")} ok={ready && !loadFailed}>
         <p className="text-[12.5px] leading-relaxed text-ink-muted">
-          {loadFailed
-            ? t("SVP's files are here but its VapourSynth engine won't load ({err}). This usually means a stale VapourSynth entry or a missing Microsoft VC++ runtime. Reinstall SVP, or install the latest \"Visual C++ Redistributable (x64)\" from Microsoft, then reopen Harbor.", { err: status?.load_error ?? "load error" })
-            : ready
-              ? t("Installed and detected. Harbor found its interpolation engine and will drive it directly.")
-              : installed
-                ? t("SVP is installed but Harbor couldn't find its engine files (svpflow + VapourSynth). Try repairing the SVP install, or reopen SVP once.")
-                : t("Install SVP once (the free tier is enough). It bundles VapourSynth + svpflow; Harbor reuses them, no extra setup.")}
+          {checking
+            ? t("Checking the local SVP and VapourSynth installation...")
+            : !supported
+              ? t(status?.reason ?? "SVP is not supported by this Harbor package.")
+              : loadFailed
+                ? t(
+                    "SVP's files are here but its VapourSynth engine won't load ({err}). This usually means a stale VapourSynth entry or a missing Microsoft VC++ runtime. Reinstall SVP, or install the latest \"Visual C++ Redistributable (x64)\" from Microsoft, then reopen Harbor.",
+                    { err: status?.load_error ?? "load error" },
+                  )
+                : ready
+                  ? linux
+                    ? t(
+                        "Installed and detected. Harbor found the native svpflow plugins and VapourSynth script library.",
+                      )
+                    : t(
+                        "Installed and detected. Harbor found its interpolation engine and will drive it directly.",
+                      )
+                  : installed
+                    ? t(
+                        "SVP is installed but Harbor couldn't find its engine files (svpflow + VapourSynth). Try repairing the SVP install, or reopen SVP once.",
+                      )
+                    : t(
+                        "Install SVP once (the free tier is enough). It bundles VapourSynth + svpflow; Harbor reuses them, no extra setup.",
+                      )}
         </p>
         <div className="flex flex-wrap items-center gap-2">
-          {installed ? (
+          {!supported ? null : installed ? (
             <button
               type="button"
               onClick={openSvp}
               disabled={busy}
               className="flex h-9 items-center gap-1.5 rounded-lg border border-edge px-3 text-[12.5px] font-semibold text-ink transition-colors hover:bg-elevated disabled:opacity-60"
             >
-              {busy ? <Loader2 size={13} className="animate-spin" /> : <Play size={13} strokeWidth={2.2} />}
+              {busy ? (
+                <Loader2 size={13} className="animate-spin" />
+              ) : (
+                <Play size={13} strokeWidth={2.2} />
+              )}
               {t("Open SVP")}
             </button>
           ) : (
-            <LinkButton label={t("Get SVP (free)")} url="https://www.svp-team.com/get/" />
+            <LinkButton
+              label={t("Get SVP (free)")}
+              url={
+                linux ? "https://www.svp-team.com/wiki/SVP:Linux" : "https://www.svp-team.com/get/"
+              }
+            />
           )}
         </div>
       </Step>
@@ -87,11 +129,26 @@ export function SvpSection() {
         label={t("Enable SVP")}
         sub={
           ready
-            ? t("Harbor's player applies the interpolation itself, embedded like normal playback, and starts SVP Manager in the tray for licensing. Restart playback to apply. If video goes black or won't start, turn this off.")
-            : t("Finish the install above first. Flipping this on now won't do anything until Harbor can find SVP's engine.")
+            ? linux
+              ? t(
+                  "Harbor loads the native svpflow filter through VapourSynth and starts SVP Manager when available. Restart playback to apply.",
+                )
+              : t(
+                  "Harbor's player applies the interpolation itself, embedded like normal playback, and starts SVP Manager in the tray for licensing. Restart playback to apply. If video goes black or won't start, turn this off.",
+                )
+            : t(
+                "Finish the install above first. Flipping this on now won't do anything until Harbor can find SVP's engine.",
+              )
         }
         value={settings.playerSvp}
         onChange={(v) => void onToggle(v)}
+        lockReason={
+          checking
+            ? t("Checking SVP installation...")
+            : !supported
+              ? (status?.reason ?? t("SVP is not supported by this Harbor package."))
+              : undefined
+        }
       />
 
       {settings.playerSvp && (
@@ -107,7 +164,9 @@ export function SvpSection() {
             onChange={(v) => update({ svpScope: v as "all" | "anime" | "non-anime" })}
           />
           <p className="mt-2 text-[12.5px] leading-relaxed text-ink-subtle">
-            {t("Frame interpolation shines on anime but can look off on live-action film. Limit it to the content you want, then restart playback.")}
+            {t(
+              "Frame interpolation shines on anime but can look off on live-action film. Limit it to the content you want, then restart playback.",
+            )}
           </p>
         </div>
       )}

2-sa/Bear

src/views/settings/anime-panel/svp-section.tsx
@@ -2,6 +2,7 @@ import { Check, ExternalLink, Loader2, Play } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useSettings } from "@/lib/settings";
 import { useT } from "@/lib/i18n";
+import { isLinuxDesktop } from "@/lib/platform";
 import { openUrl } from "@/lib/window";
 import { svpApply, svpLaunch, svpStatus, type SvpStatus } from "@/lib/svp";
 import { Section, ToggleRow, Segmented } from "../shared";
@@ -14,11 +15,16 @@ export function SvpSection() {
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    svpStatus().then(setStatus).catch(() => {});
+    svpStatus()
+      .then(setStatus)
+      .catch(() => {});
   }, []);
 
   const installed = status?.installed ?? false;
   const ready = status?.ready ?? false;
+  const checking = status === null;
+  const supported = status?.supported ?? false;
+  const linux = isLinuxDesktop();
   const loadFailed = ready && status?.loadable === false;
 
   const openSvp = async () => {
@@ -43,7 +49,9 @@ export function SvpSection() {
     try {
       const vpy = await svpApply("60");
       update({ playerSvp: true, svpVpyPath: vpy });
-      svpStatus().then(setStatus).catch(() => {});
+      svpStatus()
+        .then(setStatus)
+        .catch(() => {});
     } catch (e) {
       setError(t("Couldn't set up SVP: {err}", { err: String(e) }));
     } finally {
@@ -54,31 +62,65 @@ export function SvpSection() {
   return (
     <Section
       title={t("SVP frame interpolation")}
-      subtitle={t("Genuine 48/60fps motion on anime, rendered right inside Bear's player. SVP supplies the engine (VapourSynth + svpflow) and runs in your tray for licensing; Bear's own player applies the interpolation, so it stays embedded and fully under your control. One-time install, then flip it on.")}
+      subtitle={
+        linux
+          ? t(
+              "Native 48/60fps motion through your Linux SVP and VapourSynth installation, rendered inside Bear's embedded player.",
+            )
+          : t(
+              "Genuine 48/60fps motion on anime, rendered right inside Bear's player. SVP supplies the engine (VapourSynth + svpflow) and runs in your tray for licensing; Bear's own player applies the interpolation, so it stays embedded and fully under your control. One-time install, then flip it on.",
+            )
+      }
     >
       <Step n={1} title={t("SVP (free)")} ok={ready && !loadFailed}>
         <p className="text-[12.5px] leading-relaxed text-ink-muted">
-          {loadFailed
-            ? t("SVP's files are here but its VapourSynth engine won't load ({err}). This usually means a stale VapourSynth entry or a missing Microsoft VC++ runtime. Reinstall SVP, or install the latest \"Visual C++ Redistributable (x64)\" from Microsoft, then reopen Bear.", { err: status?.load_error ?? "load error" })
-            : ready
-              ? t("Installed and detected. Bear found its interpolation engine and will drive it directly.")
-              : installed
-                ? t("SVP is installed but Bear couldn't find its engine files (svpflow + VapourSynth). Try repairing the SVP install, or reopen SVP once.")
-                : t("Install SVP once (the free tier is enough). It bundles VapourSynth + svpflow; Bear reuses them, no extra setup.")}
+          {checking
+            ? t("Checking the local SVP and VapourSynth installation...")
+            : !supported
+              ? t(status?.reason ?? "SVP is not supported by this Bear package.")
+              : loadFailed
+                ? t(
+                    "SVP's files are here but its VapourSynth engine won't load ({err}). This usually means a stale VapourSynth entry or a missing Microsoft VC++ runtime. Reinstall SVP, or install the latest \"Visual C++ Redistributable (x64)\" from Microsoft, then reopen Bear.",
+                    { err: status?.load_error ?? "load error" },
+                  )
+                : ready
+                  ? linux
+                    ? t(
+                        "Installed and detected. Bear found the native svpflow plugins and VapourSynth script library.",
+                      )
+                    : t(
+                        "Installed and detected. Bear found its interpolation engine and will drive it directly.",
+                      )
+                  : installed
+                    ? t(
+                        "SVP is installed but Bear couldn't find its engine files (svpflow + VapourSynth). Try repairing the SVP install, or reopen SVP once.",
+                      )
+                    : t(
+                        "Install SVP once (the free tier is enough). It bundles VapourSynth + svpflow; Bear reuses them, no extra setup.",
+                      )}
         </p>
         <div className="flex flex-wrap items-center gap-2">
-          {installed ? (
+          {!supported ? null : installed ? (
             <button
               type="button"
               onClick={openSvp}
               disabled={busy}
               className="flex h-9 items-center gap-1.5 rounded-lg border border-edge px-3 text-[12.5px] font-semibold text-ink transition-colors hover:bg-elevated disabled:opacity-60"
             >
-              {busy ? <Loader2 size={13} className="animate-spin" /> : <Play size={13} strokeWidth={2.2} />}
+              {busy ? (
+                <Loader2 size={13} className="animate-spin" />
+              ) : (
+                <Play size={13} strokeWidth={2.2} />
+              )}
               {t("Open SVP")}
             </button>
           ) : (
-            <LinkButton label={t("Get SVP (free)")} url="https://www.svp-team.com/get/" />
+            <LinkButton
+              label={t("Get SVP (free)")}
+              url={
+                linux ? "https://www.svp-team.com/wiki/SVP:Linux" : "https://www.svp-team.com/get/"
+              }
+            />
           )}
         </div>
       </Step>
@@ -87,11 +129,26 @@ export function SvpSection() {
         label={t("Enable SVP")}
         sub={
           ready
-            ? t("Bear's player applies the interpolation itself, embedded like normal playback, and starts SVP Manager in the tray for licensing. Restart playback to apply. If video goes black or won't start, turn this off.")
-            : t("Finish the install above first. Flipping this on now won't do anything until Bear can find SVP's engine.")
+            ? linux
+              ? t(
+                  "Bear loads the native svpflow filter through VapourSynth and starts SVP Manager when available. Restart playback to apply.",
+                )
+              : t(
+                  "Bear's player applies the interpolation itself, embedded like normal playback, and starts SVP Manager in the tray for licensing. Restart playback to apply. If video goes black or won't start, turn this off.",
+                )
+            : t(
+                "Finish the install above first. Flipping this on now won't do anything until Bear can find SVP's engine.",
+              )
         }
         value={settings.playerSvp}
         onChange={(v) => void onToggle(v)}
+        lockReason={
+          checking
+            ? t("Checking SVP installation...")
+            : !supported
+              ? (status?.reason ?? t("SVP is not supported by this Bear package."))
+              : undefined
+        }
       />
 
       {settings.playerSvp && (
@@ -107,7 +164,9 @@ export function SvpSection() {
             onChange={(v) => update({ svpScope: v as "all" | "anime" | "non-anime" })}
           />
           <p className="mt-2 text-[12.5px] leading-relaxed text-ink-subtle">
-            {t("Frame interpolation shines on anime but can look off on live-action film. Limit it to the content you want, then restart playback.")}
+            {t(
+              "Frame interpolation shines on anime but can look off on live-action film. Limit it to the content you want, then restart playback.",
+            )}
           </p>
         </div>
       )}