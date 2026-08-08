0xhas/harbor

feat(i18n): add arabic translations for account views and catalogs

src/views/account/account-auth-form.tsx
@@ -6,6 +6,7 @@ import { accountErrorMessage } from "@/lib/account/error-messages";
 import { PasswordField, TextField } from "./fields";
 import { AccountRecoverForm } from "./account-recover-form";
 import { AccountValueProps } from "./account-value-props";
+import { useT } from "@/lib/i18n";
 
 type Mode = "signin" | "register";
 
@@ -17,6 +18,7 @@ const MODES: { id: Mode; label: string; action: string }[] = [
 const USERNAME_RE = /^[a-zA-Z0-9_]{3,24}$/;
 
 export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) => void }) {
+  const t = useT();
   const [view, setView] = useState<"auth" | "recover">("auth");
   const [mode, setMode] = useState<Mode>("register");
   const [username, setUsername] = useState("");
@@ -73,12 +75,12 @@ export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) =>
         </span>
         <div className="flex min-w-0 flex-col">
           <h3 className="font-display text-[19px] font-medium tracking-tight text-ink">
-            {mode === "register" ? "Join Harbor" : "Welcome back"}
+            {mode === "register" ? t("Join Harbor") : t("Welcome back")}
           </h3>
           <p className="text-[12.5px] text-ink-subtle">
             {mode === "register"
-              ? "One free account for your handle, themes, and sync."
-              : "Sign in to pick up where you left off."}
+              ? t("One free account for your handle, themes, and sync.")
+              : t("Sign in to pick up where you left off.")}
           </p>
         </div>
       </div>
@@ -112,20 +114,20 @@ export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) =>
           className="flex flex-col gap-4"
         >
           <TextField
-            label="Username"
+            label={t("Username")}
             value={username}
             onChange={setUsername}
-            placeholder="yourname"
+            placeholder={t("yourname")}
             maxLength={24}
             hint={usernameHint}
             tone={usernameHint ? "danger" : "muted"}
             autoComplete="username"
           />
           <PasswordField
-            label="Password"
+            label={t("Password")}
             value={password}
             onChange={setPassword}
-            placeholder={mode === "register" ? "At least 8 characters" : "Your password"}
+            placeholder={mode === "register" ? t("At least 8 characters") : t("Your password")}
             onEnter={submit}
           />
 
@@ -138,7 +140,7 @@ export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) =>
               }}
               className="-mt-1 self-end text-[12px] font-medium text-ink-subtle transition-colors hover:text-ink"
             >
-              Forgot password?
+              {t("Forgot password ?")}
             </button>
           )}
 
@@ -160,7 +162,7 @@ export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) =>
           {mode === "register" && (
             <p className="flex items-start gap-2 text-[11.5px] leading-snug text-ink-subtle">
               <KeyRound size={13} className="mt-0.5 shrink-0" />
-              We'll show a one-time recovery key right after you sign up. Save it: it's the only way back in if you forget your password.
+              {t("We'll show a one-time recovery key right after you sign up. Save it: it's the only way back in if you forget your password.")}
             </p>
           )}
         </form>

src/views/account/account-signed-in-bar.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { Loader2, LogOut } from "lucide-react";
 import { logoutAuthor, type Author } from "@/lib/theme-auth";
 import { VerifiedBadge } from "./verified-badge";
+import { useT } from "@/lib/i18n";
 
 export function AccountSignedInBar({ author }: { author: Author }) {
+  const t = useT();
   const [signingOut, setSigningOut] = useState(false);
 
   const signOut = async () => {
@@ -21,7 +23,7 @@ export function AccountSignedInBar({ author }: { author: Author }) {
           {author.verified && <VerifiedBadge />}
         </div>
         <span className="text-[12px] text-ink-subtle">
-          {author.handle ? `Signed in as ${author.username}` : "Signed in to your Harbor account"}
+          {author.handle ? t("Signed in as {username}", { username: author.username }) : t("Signed in to your Harbor account")}
         </span>
       </div>
       <button
@@ -29,7 +31,7 @@ export function AccountSignedInBar({ author }: { author: Author }) {
         disabled={signingOut}
         className="flex h-9 shrink-0 items-center gap-1.5 rounded-full border border-edge-soft px-3.5 text-[12.5px] font-medium text-ink-muted transition-colors duration-150 hover:border-danger/40 hover:text-danger disabled:opacity-50"
       >
-        {signingOut ? <Loader2 size={14} className="animate-spin" /> : <LogOut size={14} />} Sign out
+        {signingOut ? <Loader2 size={14} className="animate-spin" /> : <LogOut size={14} />} {t("Sign out")}
       </button>
     </div>
   );

src/views/account/handle-claim-card.tsx
@@ -6,6 +6,7 @@ import type { Author } from "@/lib/theme-auth";
 import { inputClass } from "./fields";
 import { useHandleAvailability, type HandleStatus } from "./use-handle-availability";
 import { HandleChangeConfirm } from "./handle-change-confirm";
+import { useT } from "@/lib/i18n";
 
 const COOLDOWN_LABEL = "once every 14 days";
 
@@ -18,6 +19,7 @@ function formatDate(ms: number): string {
 }
 
 export function HandleClaimCard({ author }: { author: Author }) {
+  const t = useT();
   const [value, setValue] = useState("");
   const [busy, setBusy] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -61,7 +63,7 @@ export function HandleClaimCard({ author }: { author: Author }) {
           <div className="flex min-w-0 flex-col">
             <span className="font-display text-[15px] text-ink">@{author.handle}</span>
             <span className="text-[12px] text-ink-subtle">
-              Locked until {formatDate(availableAt)}. You can change your handle {COOLDOWN_LABEL}.
+              {t("Locked until")} {formatDate(availableAt)}. {t("You can change your handle")} {t(COOLDOWN_LABEL)}.
             </span>
           </div>
         </div>
@@ -83,7 +85,7 @@ export function HandleClaimCard({ author }: { author: Author }) {
             setValue(e.target.value);
             setError(null);
           }}
-          placeholder={hasCustom ? author.handle ?? "yourhandle" : "yourhandle"}
+          placeholder={hasCustom ? author.handle ?? t("yourhandle") : t("yourhandle")}
           maxLength={24}
           autoComplete="off"
           autoCapitalize="off"
@@ -106,8 +108,8 @@ export function HandleClaimCard({ author }: { author: Author }) {
       <StatusLine status={status} onPick={startClaim} />
       <p className="text-[11.5px] text-ink-subtle">
         {hasCustom
-          ? `You can change your handle ${COOLDOWN_LABEL}, so pick one you'll keep.`
-          : `You can change your handle ${COOLDOWN_LABEL} after you claim it.`}
+          ? `${t("You can change your handle")} ${t(COOLDOWN_LABEL)}, ${t("so pick one you'll keep.")}`
+          : `${t("You can change your handle")} ${t(COOLDOWN_LABEL)} ${t("after you claim it.")}`}
       </p>
       {error && <p className="text-[12px] text-danger">{error}</p>}
 
@@ -125,11 +127,12 @@ export function HandleClaimCard({ author }: { author: Author }) {
 }
 
 function HandleHeader({ hasCustom }: { hasCustom: boolean }) {
+  const t = useT();
   return (
     <div className="flex flex-col gap-0.5">
-      <span className="text-[13px] font-semibold text-ink">Handle</span>
+      <span className="text-[13px] font-semibold text-ink">{t("Handle")}</span>
       <span className="text-[12px] text-ink-subtle">
-        {hasCustom ? "How people find you across Harbor." : "Claim one so people can find you across Harbor."}
+        {hasCustom ? t("How people find you across Harbor.") : t("Claim one so people can find you across Harbor.")}
       </span>
     </div>
   );
@@ -145,19 +148,20 @@ function StatusIcon({ status }: { status: HandleStatus }) {
 }
 
 function StatusLine({ status, onPick }: { status: HandleStatus; onPick: (s: string) => void }) {
+  const t = useT();
   if (status.state === "idle") return null;
-  if (status.state === "checking") return <span className="text-[11.5px] text-ink-subtle">Checking availability</span>;
+  if (status.state === "checking") return <span className="text-[11.5px] text-ink-subtle">{t("Checking availability")}</span>;
   if (status.state === "available")
-    return <span className="text-[11.5px] font-medium text-accent">That handle is yours to claim.</span>;
+    return <span className="text-[11.5px] font-medium text-accent">{t("That handle is yours to claim.")}</span>;
   if (status.state === "error")
-    return <span className="text-[11.5px] text-ink-subtle">Sign in to Harbor to check availability.</span>;
+    return <span className="text-[11.5px] text-ink-subtle">{t("Sign in to Harbor to check availability.")}</span>;
 
   const label =
     status.state === "taken"
-      ? status.reason ?? "That handle is taken."
+      ? status.reason ?? t("That handle is taken.")
       : status.state === "reserved"
-        ? status.reason ?? "That handle is reserved."
-        : status.reason ?? "That handle is not valid.";
+        ? status.reason ?? t("That handle is reserved.")
+        : status.reason ?? t("That handle is not valid.")
   const suggestions = "suggestions" in status ? status.suggestions ?? [] : [];
 
   return (

src/views/account/stremio-verify-card.tsx
@@ -6,10 +6,12 @@ import { accountErrorMessage } from "@/lib/account/error-messages";
 import { useAuth } from "@/lib/auth";
 import { canStremioWebAuth } from "@/lib/stremio-auth";
 import type { Author } from "@/lib/theme-auth";
+import { useT } from "@/lib/i18n";
 
 type Busy = "current" | "browser" | null;
 
 export function StremioVerifyCard({ author }: { author: Author }) {
+  const t = useT();
   const { authKey, user } = useAuth();
   const [busy, setBusy] = useState<Busy>(null);
   const [error, setError] = useState<string | null>(null);
@@ -19,11 +21,11 @@ export function StremioVerifyCard({ author }: { author: Author }) {
       <div className="flex items-center gap-3">
         <img src={stremioLogo} alt="Stremio" className="h-6 w-6 shrink-0" draggable={false} />
         <div className="flex min-w-0 flex-col">
-          <span className="text-[13px] font-semibold text-ink">Ownership verified</span>
-          <span className="text-[12px] text-ink-subtle">Linked to a real Stremio account.</span>
+          <span className="text-[13px] font-semibold text-ink">{t("Ownership verified")}</span>
+          <span className="text-[12px] text-ink-subtle">{t("Linked to a real Stremio account.")}</span>
         </div>
         <span className="ms-auto flex h-6 items-center gap-1 rounded-full bg-accent-soft/50 px-2.5 text-[11px] font-semibold text-accent">
-          <Check size={12} strokeWidth={3} /> Verified
+          <Check size={12} strokeWidth={3} /> {t("Verified")}
         </span>
       </div>
     );
@@ -52,13 +54,13 @@ export function StremioVerifyCard({ author }: { author: Author }) {
       <div className="flex items-center gap-3">
         <img src={stremioLogo} alt="Stremio" className="h-6 w-6 shrink-0" draggable={false} />
         <div className="flex min-w-0 flex-col">
-          <span className="text-[13px] font-semibold text-ink">Verify ownership</span>
+          <span className="text-[13px] font-semibold text-ink">{t("Verify ownership")}</span>
           <span className="truncate text-[12px] text-ink-subtle">
             {hasSession
               ? email
-                ? `Confirm you own this Stremio account (${email}).`
-                : "Confirm you own your Stremio account."
-              : "Prove you own a real Stremio account."}
+                ? t("Confirm you own this Stremio account ({email}).", { email })
+                : t("Confirm you own your Stremio account.")
+              : t("Prove you own a real Stremio account.")}
           </span>
         </div>
       </div>
@@ -66,16 +68,16 @@ export function StremioVerifyCard({ author }: { author: Author }) {
       {hasSession ? (
         <PrimaryButton busy={busy === "current"} onClick={() => run("current")} disabled={!!busy}>
           <img src={stremioLogo} alt="" className="h-4 w-4" draggable={false} />
-          {busy === "current" ? "Verifying..." : "Verify ownership"}
+          {busy === "current" ? t("Verifying...") : t("Verify ownership")}
         </PrimaryButton>
       ) : canBrowser ? (
         <PrimaryButton busy={busy === "browser"} onClick={() => run("browser")} disabled={!!busy}>
-          {busy === "browser" ? "Continue in your browser..." : "Verify with Stremio"}
+          {busy === "browser" ? t("Continue in your browser...") : t("Verify with Stremio")}
           {busy !== "browser" && <ExternalLink size={14} />}
         </PrimaryButton>
       ) : (
         <p className="rounded-xl border border-edge-soft bg-canvas/40 px-3.5 py-3 text-[12px] text-ink-subtle">
-          Open Harbor on desktop to verify ownership in your browser.
+          {t("Open Harbor on desktop to verify ownership in your browser.")}
         </p>
       )}
 
@@ -86,13 +88,13 @@ export function StremioVerifyCard({ author }: { author: Author }) {
           disabled={!!busy}
           className="self-start text-[12px] font-medium text-ink-subtle transition-colors duration-150 hover:text-ink disabled:opacity-50"
         >
-          Use a different Stremio account
+          {t("Use a different Stremio account")}
         </button>
       )}
 
       {!hasSession && canBrowser && (
         <p className="text-[11px] leading-snug text-ink-subtle">
-          Opens Stremio in your browser. Works with email, Facebook, and Apple. Harbor never sees your password.
+          {t("Opens Stremio in your browser. Works with email, Facebook, and Apple. Harbor never sees your password.")}
         </p>
       )}
 

2-sa/Bear

src/views/account/account-auth-form.tsx
@@ -6,6 +6,7 @@ import { accountErrorMessage } from "@/lib/account/error-messages";
 import { PasswordField, TextField } from "./fields";
 import { AccountRecoverForm } from "./account-recover-form";
 import { AccountValueProps } from "./account-value-props";
+import { useT } from "@/lib/i18n";
 
 type Mode = "signin" | "register";
 
@@ -17,6 +18,7 @@ const MODES: { id: Mode; label: string; action: string }[] = [
 const USERNAME_RE = /^[a-zA-Z0-9_]{3,24}$/;
 
 export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) => void }) {
+  const t = useT();
   const [view, setView] = useState<"auth" | "recover">("auth");
   const [mode, setMode] = useState<Mode>("register");
   const [username, setUsername] = useState("");
@@ -73,12 +75,12 @@ export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) =>
         </span>
         <div className="flex min-w-0 flex-col">
           <h3 className="font-display text-[19px] font-medium tracking-tight text-ink">
-            {mode === "register" ? "Join Bear" : "Welcome back"}
+            {mode === "register" ? t("Join Bear") : t("Welcome back")}
           </h3>
           <p className="text-[12.5px] text-ink-subtle">
             {mode === "register"
-              ? "One free account for your handle, themes, and sync."
-              : "Sign in to pick up where you left off."}
+              ? t("One free account for your handle, themes, and sync.")
+              : t("Sign in to pick up where you left off.")}
           </p>
         </div>
       </div>
@@ -112,20 +114,20 @@ export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) =>
           className="flex flex-col gap-4"
         >
           <TextField
-            label="Username"
+            label={t("Username")}
             value={username}
             onChange={setUsername}
-            placeholder="yourname"
+            placeholder={t("yourname")}
             maxLength={24}
             hint={usernameHint}
             tone={usernameHint ? "danger" : "muted"}
             autoComplete="username"
           />
           <PasswordField
-            label="Password"
+            label={t("Password")}
             value={password}
             onChange={setPassword}
-            placeholder={mode === "register" ? "At least 8 characters" : "Your password"}
+            placeholder={mode === "register" ? t("At least 8 characters") : t("Your password")}
             onEnter={submit}
           />
 
@@ -138,7 +140,7 @@ export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) =>
               }}
               className="-mt-1 self-end text-[12px] font-medium text-ink-subtle transition-colors hover:text-ink"
             >
-              Forgot password?
+              {t("Forgot password ?")}
             </button>
           )}
 
@@ -160,7 +162,7 @@ export function AccountAuthForm({ onRecovery }: { onRecovery?: (code: string) =>
           {mode === "register" && (
             <p className="flex items-start gap-2 text-[11.5px] leading-snug text-ink-subtle">
               <KeyRound size={13} className="mt-0.5 shrink-0" />
-              We'll show a one-time recovery key right after you sign up. Save it: it's the only way back in if you forget your password.
+              {t("We'll show a one-time recovery key right after you sign up. Save it: it's the only way back in if you forget your password.")}
             </p>
           )}
         </form>

src/views/account/account-signed-in-bar.tsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import { Loader2, LogOut } from "lucide-react";
 import { logoutAuthor, type Author } from "@/lib/theme-auth";
 import { VerifiedBadge } from "./verified-badge";
+import { useT } from "@/lib/i18n";
 
 export function AccountSignedInBar({ author }: { author: Author }) {
+  const t = useT();
   const [signingOut, setSigningOut] = useState(false);
 
   const signOut = async () => {
@@ -21,7 +23,7 @@ export function AccountSignedInBar({ author }: { author: Author }) {
           {author.verified && <VerifiedBadge />}
         </div>
         <span className="text-[12px] text-ink-subtle">
-          {author.handle ? `Signed in as ${author.username}` : "Signed in to your Bear account"}
+          {author.handle ? t("Signed in as {username}", { username: author.username }) : t("Signed in to your Bear account")}
         </span>
       </div>
       <button
@@ -29,7 +31,7 @@ export function AccountSignedInBar({ author }: { author: Author }) {
         disabled={signingOut}
         className="flex h-9 shrink-0 items-center gap-1.5 rounded-full border border-edge-soft px-3.5 text-[12.5px] font-medium text-ink-muted transition-colors duration-150 hover:border-danger/40 hover:text-danger disabled:opacity-50"
       >
-        {signingOut ? <Loader2 size={14} className="animate-spin" /> : <LogOut size={14} />} Sign out
+        {signingOut ? <Loader2 size={14} className="animate-spin" /> : <LogOut size={14} />} {t("Sign out")}
       </button>
     </div>
   );

src/views/account/handle-claim-card.tsx
@@ -6,6 +6,7 @@ import type { Author } from "@/lib/theme-auth";
 import { inputClass } from "./fields";
 import { useHandleAvailability, type HandleStatus } from "./use-handle-availability";
 import { HandleChangeConfirm } from "./handle-change-confirm";
+import { useT } from "@/lib/i18n";
 
 const COOLDOWN_LABEL = "once every 14 days";
 
@@ -18,6 +19,7 @@ function formatDate(ms: number): string {
 }
 
 export function HandleClaimCard({ author }: { author: Author }) {
+  const t = useT();
   const [value, setValue] = useState("");
   const [busy, setBusy] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -61,7 +63,7 @@ export function HandleClaimCard({ author }: { author: Author }) {
           <div className="flex min-w-0 flex-col">
             <span className="font-display text-[15px] text-ink">@{author.handle}</span>
             <span className="text-[12px] text-ink-subtle">
-              Locked until {formatDate(availableAt)}. You can change your handle {COOLDOWN_LABEL}.
+              {t("Locked until")} {formatDate(availableAt)}. {t("You can change your handle")} {t(COOLDOWN_LABEL)}.
             </span>
           </div>
         </div>
@@ -83,7 +85,7 @@ export function HandleClaimCard({ author }: { author: Author }) {
             setValue(e.target.value);
             setError(null);
           }}
-          placeholder={hasCustom ? author.handle ?? "yourhandle" : "yourhandle"}
+          placeholder={hasCustom ? author.handle ?? t("yourhandle") : t("yourhandle")}
           maxLength={24}
           autoComplete="off"
           autoCapitalize="off"
@@ -106,8 +108,8 @@ export function HandleClaimCard({ author }: { author: Author }) {
       <StatusLine status={status} onPick={startClaim} />
       <p className="text-[11.5px] text-ink-subtle">
         {hasCustom
-          ? `You can change your handle ${COOLDOWN_LABEL}, so pick one you'll keep.`
-          : `You can change your handle ${COOLDOWN_LABEL} after you claim it.`}
+          ? `${t("You can change your handle")} ${t(COOLDOWN_LABEL)}, ${t("so pick one you'll keep.")}`
+          : `${t("You can change your handle")} ${t(COOLDOWN_LABEL)} ${t("after you claim it.")}`}
       </p>
       {error && <p className="text-[12px] text-danger">{error}</p>}
 
@@ -125,11 +127,12 @@ export function HandleClaimCard({ author }: { author: Author }) {
 }
 
 function HandleHeader({ hasCustom }: { hasCustom: boolean }) {
+  const t = useT();
   return (
     <div className="flex flex-col gap-0.5">
-      <span className="text-[13px] font-semibold text-ink">Handle</span>
+      <span className="text-[13px] font-semibold text-ink">{t("Handle")}</span>
       <span className="text-[12px] text-ink-subtle">
-        {hasCustom ? "How people find you across Bear." : "Claim one so people can find you across Bear."}
+        {hasCustom ? t("How people find you across Bear.") : t("Claim one so people can find you across Bear.")}
       </span>
     </div>
   );
@@ -145,19 +148,20 @@ function StatusIcon({ status }: { status: HandleStatus }) {
 }
 
 function StatusLine({ status, onPick }: { status: HandleStatus; onPick: (s: string) => void }) {
+  const t = useT();
   if (status.state === "idle") return null;
-  if (status.state === "checking") return <span className="text-[11.5px] text-ink-subtle">Checking availability</span>;
+  if (status.state === "checking") return <span className="text-[11.5px] text-ink-subtle">{t("Checking availability")}</span>;
   if (status.state === "available")
-    return <span className="text-[11.5px] font-medium text-accent">That handle is yours to claim.</span>;
+    return <span className="text-[11.5px] font-medium text-accent">{t("That handle is yours to claim.")}</span>;
   if (status.state === "error")
-    return <span className="text-[11.5px] text-ink-subtle">Sign in to Bear to check availability.</span>;
+    return <span className="text-[11.5px] text-ink-subtle">{t("Sign in to Bear to check availability.")}</span>;
 
   const label =
     status.state === "taken"
-      ? status.reason ?? "That handle is taken."
+      ? status.reason ?? t("That handle is taken.")
       : status.state === "reserved"
-        ? status.reason ?? "That handle is reserved."
-        : status.reason ?? "That handle is not valid.";
+        ? status.reason ?? t("That handle is reserved.")
+        : status.reason ?? t("That handle is not valid.")
   const suggestions = "suggestions" in status ? status.suggestions ?? [] : [];
 
   return (

src/views/account/stremio-verify-card.tsx
@@ -6,10 +6,12 @@ import { accountErrorMessage } from "@/lib/account/error-messages";
 import { useAuth } from "@/lib/auth";
 import { canStremioWebAuth } from "@/lib/stremio-auth";
 import type { Author } from "@/lib/theme-auth";
+import { useT } from "@/lib/i18n";
 
 type Busy = "current" | "browser" | null;
 
 export function StremioVerifyCard({ author }: { author: Author }) {
+  const t = useT();
   const { authKey, user } = useAuth();
   const [busy, setBusy] = useState<Busy>(null);
   const [error, setError] = useState<string | null>(null);
@@ -19,11 +21,11 @@ export function StremioVerifyCard({ author }: { author: Author }) {
       <div className="flex items-center gap-3">
         <img src={stremioLogo} alt="Stremio" className="h-6 w-6 shrink-0" draggable={false} />
         <div className="flex min-w-0 flex-col">
-          <span className="text-[13px] font-semibold text-ink">Ownership verified</span>
-          <span className="text-[12px] text-ink-subtle">Linked to a real Stremio account.</span>
+          <span className="text-[13px] font-semibold text-ink">{t("Ownership verified")}</span>
+          <span className="text-[12px] text-ink-subtle">{t("Linked to a real Stremio account.")}</span>
         </div>
         <span className="ms-auto flex h-6 items-center gap-1 rounded-full bg-accent-soft/50 px-2.5 text-[11px] font-semibold text-accent">
-          <Check size={12} strokeWidth={3} /> Verified
+          <Check size={12} strokeWidth={3} /> {t("Verified")}
         </span>
       </div>
     );
@@ -52,13 +54,13 @@ export function StremioVerifyCard({ author }: { author: Author }) {
       <div className="flex items-center gap-3">
         <img src={stremioLogo} alt="Stremio" className="h-6 w-6 shrink-0" draggable={false} />
         <div className="flex min-w-0 flex-col">
-          <span className="text-[13px] font-semibold text-ink">Verify ownership</span>
+          <span className="text-[13px] font-semibold text-ink">{t("Verify ownership")}</span>
           <span className="truncate text-[12px] text-ink-subtle">
             {hasSession
               ? email
-                ? `Confirm you own this Stremio account (${email}).`
-                : "Confirm you own your Stremio account."
-              : "Prove you own a real Stremio account."}
+                ? t("Confirm you own this Stremio account ({email}).", { email })
+                : t("Confirm you own your Stremio account.")
+              : t("Prove you own a real Stremio account.")}
           </span>
         </div>
       </div>
@@ -66,16 +68,16 @@ export function StremioVerifyCard({ author }: { author: Author }) {
       {hasSession ? (
         <PrimaryButton busy={busy === "current"} onClick={() => run("current")} disabled={!!busy}>
           <img src={stremioLogo} alt="" className="h-4 w-4" draggable={false} />
-          {busy === "current" ? "Verifying..." : "Verify ownership"}
+          {busy === "current" ? t("Verifying...") : t("Verify ownership")}
         </PrimaryButton>
       ) : canBrowser ? (
         <PrimaryButton busy={busy === "browser"} onClick={() => run("browser")} disabled={!!busy}>
-          {busy === "browser" ? "Continue in your browser..." : "Verify with Stremio"}
+          {busy === "browser" ? t("Continue in your browser...") : t("Verify with Stremio")}
           {busy !== "browser" && <ExternalLink size={14} />}
         </PrimaryButton>
       ) : (
         <p className="rounded-xl border border-edge-soft bg-canvas/40 px-3.5 py-3 text-[12px] text-ink-subtle">
-          Open Bear on desktop to verify ownership in your browser.
+          {t("Open Bear on desktop to verify ownership in your browser.")}
         </p>
       )}
 
@@ -86,13 +88,13 @@ export function StremioVerifyCard({ author }: { author: Author }) {
           disabled={!!busy}
           className="self-start text-[12px] font-medium text-ink-subtle transition-colors duration-150 hover:text-ink disabled:opacity-50"
         >
-          Use a different Stremio account
+          {t("Use a different Stremio account")}
         </button>
       )}
 
       {!hasSession && canBrowser && (
         <p className="text-[11px] leading-snug text-ink-subtle">
-          Opens Stremio in your browser. Works with email, Facebook, and Apple. Bear never sees your password.
+          {t("Opens Stremio in your browser. Works with email, Facebook, and Apple. Bear never sees your password.")}
         </p>
       )}
 